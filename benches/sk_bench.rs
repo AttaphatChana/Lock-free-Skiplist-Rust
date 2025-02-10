832661c57bb26480b403cc466118a4f50e1f3763
Attaphat Chana <attaphart@gmail.com>
2025-02-10 23:30:53 +0700

    update benching, resolve segfault issues in insert

--- benches/sk_bench.rs
+++ benches/sk_bench.rs
@@ -1,20 +1,82 @@
 use std::iter;
-use criterion::{criterion_group, criterion_main, black_box, Criterion};
+use criterion::{criterion_group, criterion_main, black_box, Criterion, Throughput, BenchmarkId};
 use rand::Rng;
-use SkipList::sk_test;
+use SkipList::{sk_test, something,import_sk};
+use crossbeam_epoch::{Guard,self as epoch};
+use skiplist::OrderedSkipList;
+
+// fn import_bench(c: &mut Criterion) {
+//     let mut group = c.benchmark_group("import_sk");
+//
+//     for size in [1, 5,10, 50,100, 500,1000, 5000,10000, 50000,100000, 500000,1000000] {
+//         //group.throughput(Throughput::Bytes(*size as u64));
+//         group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| unsafe {
+//             b.iter(|| {
+//                 import_sk(black_box(size));
+//             })
+//         });
+//     }
+//
+// }
+
 fn sk_bench(c: &mut Criterion) {
-    c.bench_function("sk_bench", |b| {
-        b.iter(|| {
-            let mut rng = rand::thread_rng();
-            let n = 1000000;
-            let sequence: Vec<i32> = (0..n).map(|_| rng.random_range(-n..n)).collect();
-                sk_test(black_box(sequence));
+    let mut group = c.benchmark_group("sk_insert_bench");
+    // c.bench_function("sk_bench", |b| {
+    //     b.iter(|| {
+    //         let mut rng = rand::rng();
+    //         let n = 1000000;
+    //         let sequence: Vec<i32> = (0..n).map(|_| rng.random_range(-n..n)).collect();
+    //             sk_test(black_box(sequence));
+    //         })
+    //     });
+    for size in [1000, 5000,10000, 50000,100000, 500000,1000000] {
+        //group.throughput(Throughput::Bytes(*size as u64));
+        group.bench_with_input(BenchmarkId::new("sk_insert",size), &size, |b, &size| unsafe {
+            b.iter(|| {
+                sk_test(black_box(size));
             })
         });
+        group.bench_with_input(BenchmarkId::new("built-in_sk_insert",size), &size, |b, &size| unsafe {
+            b.iter(|| {
+                import_sk(black_box(size));
+            })
+        });
+
+    }
+    group.finish();
+}
+
+
+fn search(c: &mut Criterion) {
+    // static KB: usize = 1024;
+
+    let mut group = c.benchmark_group("search");
+    let n = 1000000;
+    let mut sk = something::SkipList::new();
+    let mut bt = OrderedSkipList::new();
+    let guard = epoch::pin();
+    (0.. n).for_each(|i| unsafe {
+        sk.insert(&guard,i);
+    });
+    (0.. n).for_each(|i| unsafe {
+        bt.insert(i);
+    });
+    for size in [1000, 5000,10000, 50000,100000, 500000,1000000].iter() {
+        //group.throughput(Throughput::Bytes(*size as u64));
+        group.bench_with_input(BenchmarkId::new("sk_search ", size), &size, |b, &size| unsafe {
+            b.iter(|| sk.search_n(black_box(&guard),black_box(*size)));
+        });
+        group.bench_with_input(BenchmarkId::new("built-in_sk_search ", size), &size, |b, &size| unsafe {
+            b.iter(|| bt.contains(black_box(size)));
+        });
+
+    }
+    group.finish();
 }
 
 criterion_group!(
     benches,
+    search,
     sk_bench,
 );
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,102 +1,150 @@
-use std::sync::atomic::Ordering;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, SeqCst};
 use crossbeam_epoch::{self as epoch, Atomic, Guard, Owned, Shared};
-use rand::distr::Distribution;
 use rand::Rng;
-use rand_distr::StandardGeometric;
+use crate::something::SkipList;
+use skiplist::OrderedSkipList;
 
-#[derive(Debug)]
-struct SkipNode {
-    data: i32,
-    p:Vec<Atomic<SkipNode>>
-}
-#[derive(Debug)]
-pub struct SkipList {
-    head: Atomic<SkipNode>,
-    count: i32
-}
+pub mod something{
+    use std::sync::atomic::Ordering;
+    use std::sync::atomic::Ordering::{Acquire, Relaxed, SeqCst};
+    use crossbeam_epoch::{Atomic, Guard, Shared};
+    use rand::distr::Distribution;
+    use rand_distr::StandardGeometric;
 
-impl SkipList {
-    fn new() -> Self{
-        SkipList {
-            head: Atomic::new(SkipNode {
-                data: i32::MIN,
-                p:vec![Atomic::null();2],
-            }),
-            count: 1
-        }
+    #[derive(Debug)]
+    pub struct SkipNode {
+        data: i32,
+        p:Vec<Atomic<SkipNode>>
     }
-    unsafe fn insert(&mut self, guard: &Guard, ele:i32){
-        let v = StandardGeometric.sample(&mut rand::rng()) as usize;
-        let skp = Atomic::new(SkipNode {
-            data: ele,
-            p:vec![Atomic::null();v + 1],
-        });
-        self.count += 1;
 
-        let mut refer = &self.head;
 
-        let level = refer.load(Relaxed, guard).deref().p.len();
-        if v +1 > level{
-            for _ in 0..(v +1 - level){
-                refer.load(Relaxed, guard).deref_mut().p.push(Atomic::null());
+    #[derive(Debug)]
+    pub struct SkipList {
+        pub head: Atomic<SkipNode>,
+        pub count: i32
+    }
+
+
+
+
+    impl SkipList {
+        pub fn new() -> Self{
+            SkipList {
+                head: Atomic::new(SkipNode {
+                    data: i32::MIN,
+                    p:vec![Atomic::null();2],
+                }),
+                count: 1
             }
         }
-        let mut level = refer.load(Relaxed, guard).deref().p.len();
-        while level > 0{
-            let cur = &refer
-                .load(Acquire, guard).deref().p[level-1];
-            if cur.load(Relaxed, guard).is_null(){
+        pub unsafe fn insert(&mut self, guard: &Guard, ele:i32){
+            let v = StandardGeometric.sample(&mut rand::rng()) as usize;
+            let skp = Atomic::new(SkipNode {
+                data: ele,
+                p:vec![Atomic::null();v + 1],
+            });
+            self.count += 1;
 
-                if skp.load(Relaxed, guard).deref().p.len() >= level{
-                    skp.load(Relaxed, guard).deref_mut().p[level -1].swap(Shared::null(), Relaxed, guard);
-                    cur.store(skp.load(Relaxed, guard), Relaxed);
+            let mut refer = &self.head;
+
+            let mut level = refer.load(Relaxed, guard).deref().p.len();
+            let max_level = v +1;
+            if v +1 > level{
+                for _ in level..=max_level{
+                    refer.load(Relaxed, guard).deref_mut().p.push(Atomic::null());
                 }
-                level = level-1;
-                continue;
+                for i in level..(v +1){
+
+                    refer.load(Relaxed, guard).deref().p[i].store(skp.load(Relaxed, guard), Relaxed);
+                }
             }
-            let c = cur.load(Ordering::Relaxed, guard).deref().data;
-            //let z  = cur.load(SeqCst, guard).into_owned();
-            //level = 0;
-            if c > skp.load(Relaxed, guard).deref().data {
-                if skp.load(Relaxed, guard).deref().p.len() >= level{
-                    skp.load(Acquire, guard).deref_mut().p[level -1].swap(cur.load(Relaxed, guard).into_owned(), Relaxed, guard);
+            while level > 0{
+                let cur = &refer
+                    .load(Relaxed, guard).deref().p[level-1];
+                if cur.load(Relaxed, guard).is_null(){
 
-                    let s =  skp.load(Relaxed, guard).deref().p[level-1].load(Relaxed, guard).deref();
+                    if skp.load(Relaxed, guard).deref().p.len() >= level{
+                        skp.load(Relaxed, guard).deref_mut().p[level -1].swap(Shared::null(), Relaxed, guard);
+                        cur.store(skp.load(Relaxed, guard), Relaxed);
+                    }
+                    level = level-1;
+                    continue;
+                }
+                let c = cur.load(Ordering::Relaxed, guard).deref().data;
+               
+                if c == skp.load(Relaxed, guard).deref().data{
+                    break;
+                }
+                if c > skp.load(Relaxed, guard).deref().data {
+                    if skp.load(Relaxed, guard).deref().p.len() >= level{
+                        skp.load(Relaxed, guard).deref_mut().p[level -1].swap(cur.load(Relaxed, guard).into_owned(), Relaxed, guard);
 
-                    cur.swap(skp.load(Acquire, guard).into_owned(), Relaxed,guard);
+                        cur.swap(skp.load(Relaxed, guard).into_owned(), Relaxed,guard);
+
+                    }
+                    level = level-1;
+                }else{
+                    refer = cur;
 
                 }
-                level = level-1;
-            }else{
-                refer = cur;
+            }
+            // println!("END");
+        }
+        pub unsafe fn search_n(&self, guard: &Guard,data: i32) -> bool {
+            let mut refer = &self.head;
+            let mut level = refer.load(Relaxed, guard).deref().p.len();
+
+            while level > 0{
+                let cur = &refer
+                    .load(Relaxed, guard).deref().p[level-1];
+                if cur.load(Relaxed, guard).is_null(){
+                    level = level-1;
+                    continue;
+                }
+                let c = cur.load(Relaxed, guard).deref().data;
+                if c > data {
+                    level = level-1;
+                }else if c < data {
+                    refer = cur;
+                }else{
+                    return true
+                }
+            }
+            false
+
+        }
+        unsafe fn print(self, guard: &Guard){
+            let mut refer = &self.head;
+            let check = &refer.load(SeqCst,guard).deref().p[0];
+            let mut count = self.count;
+            while count > 0{
+                // println!("data: {:?}", refer.load(SeqCst,guard).deref().data );
+                // println!("tower: {:?}",&refer.load(SeqCst,guard).deref());
+                refer = &refer.load(SeqCst,guard).deref().p[0];
+                count -=1;
 
             }
-        }
-        // println!("END");
-    }
-    unsafe fn print(self, guard: &Guard){
-        //let guard = &epoch::pin();
-        let mut refer = &self.head;
-        let check = &refer.load(SeqCst,guard).deref().p[0];
-        let mut count = self.count;
-        while count > 0{
-            // println!("data: {:?}", refer.load(SeqCst,guard).deref().data );
-            // println!("tower: {:?}",&refer.load(SeqCst,guard).deref());
-            refer = &refer.load(SeqCst,guard).deref().p[0];
-            count -=1;
 
         }
-
     }
+
 }
 
-pub fn sk_test(seq: Vec<i32>){
+pub fn import_sk(n:i32){
+    let mut skp = OrderedSkipList::new();
+    let mut rng = rand::rng();
+    let sequence: Vec<i32> = (0..n).map(|_| rng.random_range(-n..n)).collect();
+    sequence.iter().for_each(|x| unsafe {
+        skp.insert(*x)
+    })
+}
+
+pub fn sk_test(n:i32){
     let guard = epoch::pin();
     let mut skp = SkipList::new();
-    // let mut rng = rand::thread_rng();
-    // let sequence: Vec<i32> = (0..n).map(|_| rng.random_range(-n..n)).collect();
-    seq.iter().for_each(|x| unsafe {
+    let mut rng = rand::thread_rng();
+    let sequence: Vec<i32> = (0..n).map(|_| rng.random_range(-n..n)).collect();
+    sequence.iter().for_each(|x| unsafe {
         skp.insert(&guard,*x)
     })
 }
